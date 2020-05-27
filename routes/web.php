9ba636ea86d021fd78926d9b960a3f70ad9ee81a
Christian Beretta <beretta.christian2602@gmail.com>
2020-05-27 18:29:52 +0200

    creazione controller e rotte

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->group(function () {
+        Route::resource('pages', 'PageController');
+});
